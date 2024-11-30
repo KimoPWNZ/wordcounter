2939da47b4b416d2620462f92f974fbd4651195b
Mikhail Yudkin <kimopwnz@gmail.com>
2024-11-30 15:16:22 +0300

    Create WordCounter.java
    First commit

---
+++ src/WordCounter.java
@@ -0,0 +1,9 @@
+public class WordCounter {  
+    public int countWords(String input) {  
+        if (input == null || input.isEmpty()) {  
+            return 0;  
+        }  
+        String[] words = input.trim().split("\\s+");  
+        return words.length;  
+    }  
+}
